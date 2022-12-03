47231a6a4909497489fbd49ee5984da7a1acc4dc
Alexey Duryagin <aiduryagih@gmail.com>
2022-12-03 17:25:32 +0300

    Initial Commit

---
+++ Work Timer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Work Timer
+//
+//  Created by Alexey Duryagin on 03/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Work Timer/Work_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_TimerApp.swift
+//  Work Timer
+//
+//  Created by Alexey Duryagin on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Work_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
